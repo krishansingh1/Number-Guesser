e0cd3a271a403f621fa6b366bea459f19446530d
Krishan Kumar <kkrishan7074@gmail.com>
2021-08-02 19:30:47 +0530

    Modified

---
+++ app.js
@@ -0,0 +1,46 @@
+
+/*
+ GAME FUNCTION:
+ -Player must guess a number between a min and max
+ -Player gets a certain amount of guesses
+ -Notify player of guesses remaining
+ -Notify the player of the correct answer if loose
+ -Let player choose to play again
+*/
+
+//Game values
+  let min = 1,
+      max = 10,
+      winningNum = 2,
+      guessLeft = 3;
+
+ //UI Elements
+ const game = document.querySelector('game'),
+       minNum = document.querySelector('.min-num'),
+       maxNum = document.querySelector('.max-num'),
+       guessBtn = document.querySelector('#guess-btn'),
+       guessInput = document.querySelector('#guess-input'),
+       message = document.querySelector('.message');
+    
+ //Assign UI min and max
+    minNum.textContent = min;
+    maxNum.textContent = max;
+
+ //Listen for guess
+   guessBtn.addEventListener('click', function() {
+       
+    let guess=parseInt(guessInput.value);
+    //console.log(guess);
+    //Validate
+     if(guess === NAN || guess < min || guess > max) {
+       
+      setMessage('Please enter a number between ${min} and ${max}')
+     }
+   });   
+
+   //Set message
+   function setMessage(msg) {
+       
+     
+   }
+
